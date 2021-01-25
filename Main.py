22d481c59b294fa1e2a6d1cd3cd7b1e27cd3ae19
Louie Harrison <louieharrison1@hotmail.co.uk>
2021-01-25 16:25:00 +0000

    blinky code

--- Main.py
+++ Main.py
@@ -1,11 +1,22 @@
+import logging
+import RPi.GPIO as GPIO
 from Parser import Parser
 from Morse import Character, Pause
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import logging
 from urllib.parse import unquote
+from time import sleep
 
-def SendToLED(message):
-    print(message)
+def SendToLED(signal):
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+     # Set pin 8 to be an output pin and set initial value to low (off)
+    GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
+    on = False
+
+    for x in signal:
+        on = not on
+        GPIO.output(8, (GPIO.HIGH if on else GPIO.LOW))
+        sleep(x)
 class S(BaseHTTPRequestHandler):
     def _set_response(self):
         self.send_response(200)
@@ -17,30 +28,24 @@ class S(BaseHTTPRequestHandler):
         p = Parser()
         coll = p.Parse(message)
         morseStr = ""
-        signalStr = ""
+        signal = []
 
-        ## Output in 'signal' format
         for char in coll:
-            
             l = len(char.units)
+            # We know the first signal is 'on' and every other/second signal is a pause so no need to differentiate, just store a list of lengths
+            signal.append(l)
             if type(char) is Character:
                 morseStr += "." if l == 1 else "-"
-                for _ in range(0, l):
-                    signalStr += "-"
             elif type(char) is Pause:
-                for _ in range(0, l):
-                    signalStr += " "
                 if l > 1:
                     morseStr += " "
                 if l > 6:
                     morseStr += "/ "         
-
-        SendToLED(signalStr)
         
         self._set_response()
 
         # Output a rep of the signal
-        self.wfile.write(signalStr.encode('utf-8'))
+        self.wfile.write((''.join([str(x) for x in signal])).encode('utf-8'))
 
         self.wfile.write("<br/>".encode('utf-8'))
         self.wfile.write("<br/>".encode('utf-8'))
@@ -53,6 +58,8 @@ class S(BaseHTTPRequestHandler):
 
         # Just output the message
         self.wfile.write(message.encode("utf-8"))
+
+        SendToLED(signal)
 
 
 def run(server_class=HTTPServer, handler_class=S, port=8080):
